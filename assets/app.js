84d79258536200c86a4322220e48e2d2b65ef921
NeusStahmer <73188115+NeusStahmer@users.noreply.github.com>
2021-03-28 20:53:19 +0200

    javascript slider

--- assets/app.js
+++ assets/app.js
@@ -10,5 +10,12 @@ popupMenuOpen.click(function () {
 
     });
 
-
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+      });
 });
