add90bb7e80c84fcf58886d0c0b2832ef0847f12
NeusStahmer <73188115+NeusStahmer@users.noreply.github.com>
2021-03-28 20:03:03 +0200

    javascript menu

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  
+var popup = $('.js-popup');    
+var popupMenuOpen = $('.js-open');
+var popupMenuClose = $('.js-close');
+
+
+popupMenuOpen.click(function () {
+    popup.show();
+
+    });
+
+
+});
