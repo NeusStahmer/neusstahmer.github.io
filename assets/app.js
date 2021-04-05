2c3096ed090b3e777d88696593df00e517ef8a5f
NeusStahmer <73188115+NeusStahmer@users.noreply.github.com>
2021-04-05 15:10:17 +0200

    popup style

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,11 @@ var popupMenuClose = $('.js-close');
 
 popupMenuOpen.click(function () {
     popup.show();
+  });
 
-    });
+    popupMenuClose.click(function () {
+      popup.hide();
+  });
 
     $('.slider').slick({
         dots: true,
