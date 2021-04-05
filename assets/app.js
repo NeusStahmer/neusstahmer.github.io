197a5fb1c424976fc706b7cd458007d3ec7e04d6
NeusStahmer <73188115+NeusStahmer@users.noreply.github.com>
2021-04-05 16:21:35 +0200

    slider second try

--- assets/app.js
+++ assets/app.js
@@ -13,12 +13,6 @@ popupMenuClose.click(function () {
       popup.hide();
   });
 
-    $('.slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        centerMode: true,
-        variableWidth: true
-      });
+  $('.slider').slick();
+
 });
